0710424056acd7db2690302f645858157bb18f32
Alek90 <4lek.90@gmail.com>
2021-10-13 15:58:47 +0200

    Creazione repo.
    Creata repo e preparata organizzazione di base dei file ritenuti necessari.

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//  FIZZBUZZDOM
+
+
+// Panoramica del problema
+/*  
+    Creare un programma che esegua un ciclo da 1 a 100;
+    Ad ogni iterazione appendere un elemento al contenitore in html;
+    diversificare tale elemento sulla base di condizioni:
+        -divisibile per 3;
+        -divisibile per 5;
+        -divisibile sia per 3 che per 5;
+ */
+
+        
